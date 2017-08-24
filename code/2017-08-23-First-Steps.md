0b2ce9e4bffbbd7605184cf7be9d2b66d9ed3f55
Matthew Traylor <mhtraylor@hotmail.com>
2017-08-23 22:07:58 -0500

    test page

--- code/2017-08-23-First-Steps.md
+++ code/2017-08-23-First-Steps.md
@@ -1,8 +1,3 @@
----
-layout: page
-title: First Steps
----
-
 Trying a new blog based on Colin Bull's FsBlog template using FSharp.Formatting. Let's see how it goes.
 
     [module=Test]
--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mono .paket/paket.exe restore
+# mono .paket/paket.exe restore
 mono tools/fsformatting.exe literate --processDirectory --lineNumbers false --inputDirectory  "code" --outputDirectory "_posts"
 
-git add --all .
-git commit -m $1
-git push
+# git add --all .
+# git commit -m $1
+# git push
